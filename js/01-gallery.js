7f4bfa334a9357ecca1ec3336e830e01a599f539
Diana Baranovska <dmitrudi@gmail.com>
2023-03-05 01:07:45 +0200

    вирішує друге завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const cards = creatGalleryCards(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', cards);
+
+galleryEl.addEventListener('click', openModalClick);
+
+function creatGalleryCards(image) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>`;
+    })
+    .join('');
+}
+let instance;
+
+function openModalClick(event) {
+  window.addEventListener('keydown', closeModalEsc);
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+}
+
+function closeModalEsc(event) {
+  if (event.code === 'Escape') {
+    window.removeEventListener('keydown', closeModalEsc);
+    instance.close();
+  }
+}
